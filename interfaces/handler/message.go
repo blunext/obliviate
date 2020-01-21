0bc1b0a32cd1c38255705df4c08f798b6134c5d1
Blunext <>
2020-01-21 17:36:35 +0100

    Cache control

--- interfaces/handler/message.go
+++ interfaces/handler/message.go
@@ -49,6 +49,9 @@ func ProcessTemplate(config *config.Configuration, publicKey string) http.Handle
 		}
 
 		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
+		w.Header().Set("Pragma", "no-cache")
+		w.Header().Set("Expires", "0")
 
 		data := translation.GetTranslation(r.Header.Get("Accept-Language"))
 		data["PublicKey"] = publicKey
