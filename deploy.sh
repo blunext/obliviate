a6eb4253503b1204e505fe2cfe2762a03d227a3c
Blunext <>
2020-07-28 22:25:53 +0200

    deploy update

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 
-#gcloud auth configure-docker
+npm run build --prefix web
 
+#connect CLI with GCM: gcloud auth configure-docker
 docker build . --tag gcr.io/obliviate/obliviate
 docker push gcr.io/obliviate/obliviate
 
