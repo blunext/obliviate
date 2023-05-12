a8623e470e7794104683471dec21b99a65e72f88
Blunext <>
2023-05-12 17:04:32 +0200

    fix for french test

--- i18n/i18n_test.go
+++ i18n/i18n_test.go
@@ -1,8 +1,9 @@
 package i18n
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 type testFields struct {
@@ -11,7 +12,7 @@ type testFields struct {
 
 var testData = []testFields{
 	{"pl-PL,pl;q=0.9,en-US;q=0.8,en;q=0.7", "Prywatne wiadomości"},
-	{"fr-CH, fr;q=0.9, en;q=0.8, de;q=0.7, *;q=0.5", "Private secure notes"},
+	{"fr-CH, fr;q=0.9, en;q=0.8, de;q=0.7, *;q=0.5", "Notes privées sécurisées"},
 	{"en-ca,en;q=0.8,en-us;q=0.6,de-de;q=0.4,de;q=0.2", "Private secure notes"},
 	{"da, en-gb:q=0.5, en:q=0.4", "Private secure notes"},
 }
