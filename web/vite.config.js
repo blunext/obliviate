736780082dfe2a925e96dd673c0c3741fe59c1c8
Blunext <info@securenote.io>
2023-10-29 09:06:17 +0100

    Disable code splitting

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,14 +1,21 @@
-import { sveltekit } from "@sveltejs/kit/vite";
-import { defineConfig } from "vite";
+import {sveltekit} from "@sveltejs/kit/vite";
+import {defineConfig} from "vite";
 
 export default defineConfig({
-  plugins: [sveltekit()],
-
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: '@use "src/variables.scss" as *;',
-      },
+// Disable code splitting
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks: () => 'app',
+            },
+        },
     },
-  },
+    plugins: [sveltekit()],
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: '@use "src/variables.scss" as *;',
+            },
+        },
+    },
 });
